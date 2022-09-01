d5c69f2942b3f112fe1600874c97225fbbff5b16
arodriguezca <arodriguezca@github.com>
2022-09-01 14:58:59 -0400

    clean up

--- utils/covid_prediction_list.py
+++ utils/covid_prediction_list.py
@@ -1,3 +1,142 @@
+# ew202201 remove
+death_remove = ['OH','KS']
+hosp_remove = [] 
+death_replace_last1 = ['IN','LA','MA','NC','PA','RI','WA','WI','WV','X']
+increase_death_interval_high = []
+increase_death_interval_low = []
+# ew202206 remove
+death_remove = ['OH']
+hosp_remove = [] 
+death_replace_last1 = ['ND','LA']
+increase_death_interval_high = []
+increase_death_interval_low = []
+# ew202207 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['AZ']
+increase_death_interval_high = []
+increase_death_interval_low = []
+# ew202209 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['X','NJ','MS']
+increase_death_interval_high = []
+increase_death_interval_low = []
+
+# ew202210 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+increase_death_interval_high = []
+increase_death_interval_low = []
+
+# ew202211 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last2 = ['NV','NY']
+death_replace_last1 = ['CA','AR','CT','IL','LA','NJ','X']
+
+# ew202212 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last2 = ['NY','PA','NC','MN','MI','LA','NJ','PA']
+death_replace_last1 = ['X','CA','CO','GA']
+
+# ew202213 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last2 = ['CO','DC','ND','PA']
+death_replace_last3 = ['CA','IL','NC','NJ','NY']
+death_replace_last1 = []
+
+# ew202214 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last2 = ['IN','VT']
+death_replace_last1 = ['AL','LA','MA','PA','SC','SD','TX']
+
+# ew202215 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last2 = []
+death_replace_last1 = []
+
+# ew202216 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['TX','VT']
+death_replace_last2 = []
+death_replace_last3 = ['X','RI']
+
+# ew202217 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = ['AZ']
+
+# ew202218 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['IL','MD','IN','MA']
+death_replace_last2 = ['CA','IA','OH']
+death_replace_last3 = []
+
+# ew202219 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202220 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202221 remove
+death_remove = ['CA']
+hosp_remove = []    
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = ['CA']
+
+# ew202222 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['FL']
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202223 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202224 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = ['CA','IL','SD','FL']
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202225 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = []
+
+# ew202226 remove
+death_remove = []
+hosp_remove = [] 
+death_replace_last1 = []
+death_replace_last2 = []
+death_replace_last3 = []
 
 # ew202227 remove
 death_remove = []
